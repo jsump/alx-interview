eaeadb822a535ed20b8a6e8a200a32c39dbc35a1
jsump <libbyn79@gmail.com>
2024-05-16 18:54:04 +0300

    fix code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,26 +7,24 @@ Prime game
 
 def isWinner(x, nums):
     """
-    This function takes turns between Ben and maria to prick prime numners.
-    They take turns choosing a prime number from the set,
-    and removing that number and its multiples from the set.
-    The player that cannot make a move loses the game.
+    Determines the winner of the prime game.
 
-    x: the number of rounds
-    nums: an array of n
-    Return: name of the player that won the most rounds
+    Args:
+    - x: the number of rounds
+    - nums: an array of n for each round
 
-    If the winner cannot be determined, return None
-    You can assume n and x will not be larger than 10000
-    You cannot import any packages in this task maria_wins = 0
+    Returns:
+    - The name of the player that won the most rounds.
+      If the winner cannot be determined, returns None.
     """
+
     ben_wins = 0
     maria_wins = 0
 
-    max_number = max(nums)
-    primes = generate_primes(max_number)
-
     for n in nums:
+        # Generate primes using Sieve of Eratosthenes
+        primes = generate_primes(n)
+        
         # Check if n is 1 or a prime number
         if n == 1 or n in primes:
             maria_wins += 1
@@ -40,18 +38,23 @@ def isWinner(x, nums):
     else:
         return None
 
-
 def generate_primes(n):
     """
-    This function generates prime numbers up to a given number
-    using 
+    Generates prime numbers up to a given number using Sieve of Eratosthenes.
+
+    Args:
+    - n: the upper limit for generating primes
+
+    Returns:
+    - A set containing prime numbers up to n
     """
-    primes = []
+
+    primes = set()
     sieve = [True] * (n+1)
     sieve[0] = sieve[1] = False
     for p in range(2, int(n**0.5)+1):
         if sieve[p]:
-            primes.append(p)
+            primes.add(p)
             for i in range(p*p, n+1, p):
                 sieve[i] = False
     return primes
---
+++ 0x0A-primegame/main_test.py
@@ -0,0 +1,30 @@
+import unittest
+isWinner = __import__('0-prime_game').isWinner
+
+class TestPrimeGame(unittest.TestCase):
+    def test_single_round_maria_wins(self):
+        self.assertEqual(isWinner(1, [2]), "Maria")
+
+    def test_single_round_ben_wins(self):
+        self.assertEqual(isWinner(1, [4]), "Ben")
+
+    def test_multiple_rounds_maria_wins(self):
+        self.assertEqual(isWinner(3, [2, 3, 5]), "Maria")
+
+    def test_multiple_rounds_ben_wins(self):
+        self.assertEqual(isWinner(3, [4, 6, 8]), "Ben")
+
+    def test_tie(self):
+        self.assertEqual(isWinner(2, [4, 5]), None)
+
+    def test_empty_nums(self):
+        self.assertEqual(isWinner(1, []), None)
+
+    def test_large_numbers_maria_wins(self):
+        self.assertEqual(isWinner(1, [9999]), "Maria")
+
+    def test_large_numbers_ben_wins(self):
+        self.assertEqual(isWinner(1, [10000]), "Ben")
+
+if __name__ == "__main__":
+    unittest.main()
